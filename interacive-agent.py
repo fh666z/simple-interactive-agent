1336e468c11d2525561f8f03e2e5b375eac7ac83
fh666z <fh666z@gmail.com>
2026-01-12 13:08:26 +0200

    Enhance interactive agent by implementing tool mapping and adding query handling for arithmetic operations

--- interacive-agent.py
+++ interacive-agent.py
@@ -29,8 +29,36 @@ def multiply(a: int, b:int) -> int:
     return a * b
 
 tools = [add, subtract, multiply]
-llm = init_chat_model(model="gemini/gemini-2.5-flash", model_provider="google_genai")
+tool_map = {
+    "add": add, 
+    "subtract": subtract,
+    "multiply": multiply
+}
+llm = init_chat_model(model="gemini-2.5-flash", model_provider="google_genai")
 
 
 llm_with_tools = llm.bind_tools(tools)
 
+query = "What is 3 + 2?"
+chat_history = [HumanMessage(content=query)]
+
+response_1 = llm_with_tools.invoke(chat_history)
+chat_history.append(response_1)
+
+#print(type(response_1))
+#response_1.pretty_print()
+
+tool_calls_1 = response_1.tool_calls
+
+tool_1_name = tool_calls_1[0]["name"]
+tool_1_args = tool_calls_1[0]["args"]
+tool_call_1_id = tool_calls_1[0]["id"]
+
+tool_response = tool_map[tool_1_name].invoke(tool_1_args)
+tool_message = ToolMessage(content=tool_response, tool_call_id=tool_call_1_id)
+
+chat_history.append(tool_message)
+
+answer = llm_with_tools.invoke(chat_history)
+print(type(answer))
+print(answer.content)
